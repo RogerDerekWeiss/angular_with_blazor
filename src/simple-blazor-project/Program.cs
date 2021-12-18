a3eb5e4207e72bfe865a7e179bd3c880aeed4ab0
Roger Weiss <rogerderek.weiss@absa.africa>
2021-12-18 12:59:18 +0200

    Added BlazorInfo.razor file for component generation

--- src/simple-blazor-project/Program.cs
+++ src/simple-blazor-project/Program.cs
@@ -1,3 +1,4 @@
+using JSComponentGeneration.Angular;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using simple_blazor_project;
@@ -5,6 +6,7 @@ using simple_blazor_project;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.RegisterForAngular<BlazorInfo>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
